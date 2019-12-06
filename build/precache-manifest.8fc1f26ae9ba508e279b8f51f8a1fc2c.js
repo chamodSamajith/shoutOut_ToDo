e45eb4e8cbaf6f8e33d427cf3865a7ca217927f7
Chamod Rathnayake <52029051+chamodSamajith@users.noreply.github.com>
2019-12-06 21:40:05 +0530

    Add files via upload

---
+++ build/precache-manifest.8fc1f26ae9ba508e279b8f51f8a1fc2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c21758b304be142513fb5f4d2120f9e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9c0502328f43c9acfb3",
+    "url": "/static/css/main.36549e16.chunk.css"
+  },
+  {
+    "revision": "de92b1466a11a7031149",
+    "url": "/static/js/2.4c7c5984.chunk.js"
+  },
+  {
+    "revision": "c9c0502328f43c9acfb3",
+    "url": "/static/js/main.fb2606fa.chunk.js"
+  },
+  {
+    "revision": "6309b55f35386008bdd9",
+    "url": "/static/js/runtime-main.d55adb39.js"
+  }
+]);
